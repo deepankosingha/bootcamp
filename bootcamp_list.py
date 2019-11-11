6a0ca11e3dca16a232552184b4a757c9eeae9f39
deepankosingha <32492539+deepankosingha@users.noreply.github.com>
2019-11-12 09:44:26 +1100

    Add files via upload

---
+++ bootcamp_list.py
@@ -0,0 +1,18 @@
+""" program to find even product and odd sum
+    By: Deepanko Singha
+    ID: 30479851
+"""
+n = int(input("enter no of items: "))
+li = []
+for i in range(n):
+    user_item = int(input('enter integer: '))
+    # adding the items to the list
+    li.append(user_item)
+even_odd_list = []
+for i in range(n - 1, 0, -1):
+    for j in range(1, n):
+        # checking that the product is even and sum of elements is odd
+        if ((li[i] * li[j]) % 2 == 0) and ((li[i] + li[j]) % 2 != 0):
+            even_odd_list.append(li[i])
+            even_odd_list.append(li[j])
+print("the list with even product and odd sum: \n", even_odd_list)
